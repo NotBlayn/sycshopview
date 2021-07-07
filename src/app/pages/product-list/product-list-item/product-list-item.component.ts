c3a79111e68a8afb226d86d751aaedd14015eea3
Fatih <yildizfatih96@gmail.com>
2021-07-07 12:31:46 +0200

    product-list view completed with cards

--- src/app/pages/product-list/product-list-item/product-list-item.component.ts
+++ src/app/pages/product-list/product-list-item/product-list-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProductStockService } from 'src/app/services/product-stock.service';
 import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
@@ -6,14 +8,44 @@ import { ProductsService } from 'src/app/services/products.service';
   templateUrl: './product-list-item.component.html',
   styleUrls: ['./product-list-item.component.scss']
 })
-export class ProductListItemComponent implements OnInit {
+export class ProductListItemComponent implements OnInit, OnDestroy {
 
   @Input() product!: any;
-  @Input() brand!: any;
+  nameNumberCharacters: boolean;
+  brandNameNumberCharacters: boolean;
 
-  constructor(private productService: ProductsService) { }
+  productStockByProducuctIdSubscription!: Subscription;
+
+  // quantity: number;
+  productStockByProduct: any;
+
+  constructor(private productService: ProductsService, private productStockService: ProductStockService) {
+    this.nameNumberCharacters = false;
+    this.brandNameNumberCharacters = false;
+  }
 
   ngOnInit(): void {
+    this.productStockByProducuctIdSubscription = this.productStockService.productStockByProductIdSubject
+      .subscribe((productStock: any) => {
+        this.productStockByProduct = productStock
+      });
+    this.productStockByProduct = this.productStockService.getProductStockByProductId(this.product.id);
+    this.resizeToFit();
+  }
+
+  ngOnDestroy(): void {
+    this.productStockByProducuctIdSubscription?.unsubscribe();
+  };
+
+  resizeToFit() {
+    const productName: string = this.product.name;
+    const productBrandName: string = this.product.brand.name;
+    if (productName.length > 22) {
+      this.nameNumberCharacters = true;
+    }
+    if (productBrandName.length > 22) {
+      this.brandNameNumberCharacters = true;
+    }
   }
 
 }
